1006ed37a3245b3fed489d2acad41ccb0fdb422c
SlavaYES <voen1999@gmail.com>
2018-04-24 20:26:45 +0700

    Full made input file

--- src/add_tem.c
+++ src/add_tem.c
@@ -1,93 +1,126 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <malloc.h>
+#include <malloc.h>//watching не надо в винда
 #include "add_tem.h"
-#include "conios.h"
-#define new_memory (Otvet *)malloc(sizeof(Otvet));
+#include "conios.h"//watching <conio.h>
 
-typedef struct {
-    char question[50];
-    char answer[100];
-    char num[10];
-} Full_fild;
+typedef struct Record {
+    char quest[50];
+    char answer[150];
+    int num[10];
+} Record;
 
-typedef struct Otvet {
-    struct Otvet *next;
-    char data[16];
-} Otvet;
-/*
-typedef struct Quest{
-    struct Quest *next;
-    char quest[128];
-} Quest;
-*/
-void setTem(void)
+void set_theme(void)
 {
-    /*TEM*/
-
-    char full_name[30];
-    char name_tem[100];
-    char syfix[5];
-
+    /*THEME*/
+    FILE *file;
+    int flag, i, num[1];
+    char name_file[11], full_name[20];
+    char name_theme[100], suffix[5];
+    char the_s[4], que_s[4], ans_s[4], try_s[4];
+    strcpy(the_s, "##\n");
+    strcpy(que_s, "#?\n");
+    strcpy(ans_s, "#!\n");
+    strcpy(try_s, "#^\n");
     strcpy(full_name, "txt/");
-    strcpy(syfix, ".txt");
-
-    clrscr();
-    printf("Введите Тему Теста:\n"); fgets(name_tem, 100, stdin);
-    while (strlen(name_tem)>20) {
+    strcpy(suffix, ".txt");
+    flag=0;
+    do {
 	clrscr();
-	printf("\x1b[31mОшибка ввода данных,\n");
-	printf("Вы превысили лимит символов [20]\n");
-	printf("Попробуйте еще раз...\x1b[0m\n");
-	printf("Введите Тему Теста:\n"); fgets(name_tem, 100, stdin);
-    }
-    clrscr();
-    strcat(full_name, name_tem);
-    memmove(full_name+strlen(full_name)-1, syfix, strlen(full_name)+strlen(syfix));
-    /*TEM*/
-    /*QUESTION*/
-    //Quest *head_q, *tail_q, *point_q;
-    //char exit, question[512];
-    //int k;
+	if (flag) printf("Ошибка ввода данных...\n");
+	printf("Введите Имя Файла[10]:\n");
+	scanf("%s", name_file);
+	flag=1;
+    } while (strlen(name_file) > 10);
     
+    strcat(full_name, name_file);
+    strcat(full_name, suffix);
+    
+    file = fopen(full_name, "wb");
+    if (file == 0) {
+	printf("Ошибка открытия файла\n");
+	return;
+    }
+    flag=0;
+    do {
+	clrscr();
+	if (flag) printf("Ошибка ввода данных...\n");
+	printf("Введите Тему Теста[20]:\n");
+	scanf("%s", name_theme);
+	flag=1;
+    } while (strlen(name_theme) > 20);
+    fwrite(the_s, 1, 2, file);
+    fwrite(name_theme, 1, strlen(name_theme), file);
+    fwrite(the_s, 1, 3, file);
+    /*THEME*/
     /*QUESTION*/
+    
     /*IN WORKING*/
-    Otvet *head_a, *tail_a, *point_a;
-    char exit, answer[128];
-    int i=1;
-    head_a=new_memory;
-    head_a->next=NULL;
-    tail_a=(Otvet *)&head_a;
-
+    Record record;
+    char exit;
+    exit='n';
     while (exit!='y') {
-	printf("Введите (%d) ответ\t", i);
-	fgets(answer, 50, stdin);
-	if (strlen(answer)<20) {
-	    point_a=new_memory;
-	    strcpy(point_a->data, answer);
-	    tail_a->next=point_a;
-	    tail_a=point_a;
+	/*QUESTION*/
+	flag=0;
+	do {
+	    clrscr();
+	    if (flag) printf("Ошибка ввода данных...\n");
+	    printf("Введите Вопрос[100]:\n");
+	    scanf("%s", record.quest);
+	    flag=1;
+	} while (strlen(record.quest) > 100);
+	fwrite(que_s, 1, 2, file);
+	fwrite(record.quest, 1, strlen(record.quest), file);
+	fwrite(que_s, 1, 3, file);
+	/*QUESTION*/
+	/*ANSWER*/
+	i=1;
+	fwrite(ans_s, 1, 2, file);
+	while (exit!='y') {
+	    flag=0;
+	    do {
+		clrscr();
+		if (flag) printf("Ошибка ввода данных...\n");
+		printf("Введите (%d) ответ[10]:\n", i);
+		scanf("%s", record.answer);
+		flag=1;
+	    } while (strlen(record.answer) > 10);
+	    num[0]=i;
+	    fwrite(num, 4, 1, file);
+	    fputs(")", file);
+	    fwrite(record.answer, 1, strlen(record.answer), file);
+	    fputs(". ", file);
 	    i++;
-	    printf("Закончить ввод ответов?(y/n):\n");
+	    printf("Закончить ввод ответов?[y]...\n");
+	    getch();
 	    exit=getch();
-	} else {
-	    printf("\x1b[31mОшибка ввода данных,\n");
-	    printf("Ответ не может содержать более 20 символов\n");
-	    printf("Попробуйте еще раз...\x1b[0m\n");
 	}
-    }
-    point_a=head_a;
-    while (point_a) {
-	printf("%s", point_a->data);
-	point_a=point_a->next;
-    }
-    while (head_a) {
-	point_a=head_a;
-	head_a=point_a->next;
-	free(point_a);
+	fwrite(ans_s, 1, 3, file);
+	/*ANSWER*/
+	/*TRY NUM*/
+	printf("Введите правильный(ые) номер(а) ответа(ов):\n");
+	i=1;
+	fwrite(try_s, 1, 2, file);
+	exit='n';
+	while (exit != 'y') {
+	    printf("Ответ: ");
+	    scanf("%d", record.num+i);
+	    i++;
+	    printf("Закончить ввод?[y]...\n");
+	    getch();
+	    exit=getch();
+	}
+	fwrite(record.num, sizeof(int), i, file);
+	fwrite(try_s, 1, 3, file);
+	/*TRY NUM*/
+	printf("Закончить ввод вопросов?[y]...\n");
+	exit=getch();
     }
     /*IN WORKING*/
+    fclose(file);
+    /*QUESTION*/
 }
+
 
 
