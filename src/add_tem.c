a203a20cc440813b52c53de52d924cae986cff3c
SlavaYES <voen1999@gmail.com>
2018-04-26 21:40:05 +0700

    Error work input data - function fgets() and getchar()

--- src/add_tem.c
+++ src/add_tem.c
@@ -1,9 +1,21 @@
+/*
+Проблемы с вводом данных
+После выполнения fputs или printf, fgets ведет себя не понятно,
+Вместо того чтобы получить с клавиатуры данные, он их откуда-то получает, и заканчивает ввод данных.
+(Догадываюсь, что из предыдущей строки видит символ конца строки)
+getchar работает точно так же, не дает мне ввести данные
+Писал отделную программу для ввода, как в основной -> всё работает нормально
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <malloc.h>//watching не надо в винда
 #include "add_tem.h"
-#include "conios.h"//watching <conio.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include "conios.h"
+#endif
 
 typedef struct Record {
     char quest[50];
@@ -13,77 +25,80 @@ typedef struct Record {
 
 void set_theme(void)
 {
-    /*THEME*/
     FILE *file;
-    int flag, i, num[1];
-    char name_file[11], full_name[20];
+
+    int flag, i, j, num[1];
+    char name_file[11], full_name[30];
     char name_theme[100], suffix[5];
     char the_s[4], que_s[4], ans_s[4], try_s[4];
+    /*Symboles*/
     strcpy(the_s, "##\n");
     strcpy(que_s, "#?\n");
     strcpy(ans_s, "#!\n");
     strcpy(try_s, "#^\n");
     strcpy(full_name, "txt/");
     strcpy(suffix, ".txt");
+    /*Name file*/
     flag=0;
     do {
 	clrscr();
-	if (flag) printf("Ошибка ввода данных...\n");
-	printf("Введите Имя Файла[10]:\n");
+	if (flag) perror("Ошибка ввода данных...\n");
+	fputs("Введите Имя Файла[10]:\n", stdout);
 	scanf("%s", name_file);
 	flag=1;
     } while (strlen(name_file) > 10);
-    
     strcat(full_name, name_file);
     strcat(full_name, suffix);
-    
     file = fopen(full_name, "wb");
-    if (file == 0) {
-	printf("Ошибка открытия файла\n");
+    if (!file) {
+	perror("Ошибка открытия файла\n");
 	return;
     }
+    /*Theme*/
     flag=0;
     do {
 	clrscr();
-	if (flag) printf("Ошибка ввода данных...\n");
-	printf("Введите Тему Теста[20]:\n");
-	scanf("%s", name_theme);
+	if (flag) perror("Ошибка ввода данных...\n");
+	fputs("Введите Тему Теста[20]:\n", stdout);
+	fgets(name_theme, 100, stdin);
 	flag=1;
     } while (strlen(name_theme) > 20);
     fwrite(the_s, 1, 2, file);
     fwrite(name_theme, 1, strlen(name_theme), file);
     fwrite(the_s, 1, 3, file);
-    /*THEME*/
     /*QUESTION*/
-    
-    /*IN WORKING*/
     Record record;
     char exit;
     exit='n';
+    j=1;
     while (exit!='y') {
-	/*QUESTION*/
+	/*Question*/
 	flag=0;
 	do {
-	    clrscr();
-	    if (flag) printf("Ошибка ввода данных...\n");
-	    printf("Введите Вопрос[100]:\n");
-	    scanf("%s", record.quest);
+	    //clrscr();
+	    if (flag) perror("Ошибка ввода данных...\n");
+	    printf("Введите (%d) Вопрос[100]:\n", j);
+	    fgets(record.quest, 150, stdin);
 	    flag=1;
 	} while (strlen(record.quest) > 100);
+	num[0]=j;
 	fwrite(que_s, 1, 2, file);
+	fwrite(num, 4, 1, file);
 	fwrite(record.quest, 1, strlen(record.quest), file);
 	fwrite(que_s, 1, 3, file);
+	j++;
 	/*QUESTION*/
 	/*ANSWER*/
 	i=1;
 	fwrite(ans_s, 1, 2, file);
+	exit='n';
 	while (exit!='y') {
 	    flag=0;
 	    do {
-		clrscr();
-		if (flag) printf("Ошибка ввода данных...\n");
+		//clrscr();
+		if (flag) perror("Ошибка ввода данных...\n");
 		printf("Введите (%d) ответ[10]:\n", i);
-		scanf("%s", record.answer);
+		fgets(record.answer, 50, stdin);
 		flag=1;
 	    } while (strlen(record.answer) > 10);
 	    num[0]=i;
@@ -92,30 +107,29 @@ void set_theme(void)
 	    fwrite(record.answer, 1, strlen(record.answer), file);
 	    fputs(". ", file);
 	    i++;
-	    printf("Закончить ввод ответов?[y]...\n");
-	    getch();
-	    exit=getch();
+	    fputs("Закончить ввод ответов?[y]...\n", stdout);
+	    exit=getchar();
 	}
 	fwrite(ans_s, 1, 3, file);
+	
 	/*ANSWER*/
 	/*TRY NUM*/
-	printf("Введите правильный(ые) номер(а) ответа(ов):\n");
+	fputs("Введите правильный(ые) номер(а) ответа(ов):\n", stdout);
 	i=1;
 	fwrite(try_s, 1, 2, file);
 	exit='n';
 	while (exit != 'y') {
-	    printf("Ответ: ");
+	    fputs("Ответ: ", stdout);
 	    scanf("%d", record.num+i);
 	    i++;
-	    printf("Закончить ввод?[y]...\n");
-	    getch();
-	    exit=getch();
+	    fputs("Закончить ввод?[y]...\n", stdout);
+	    exit=getchar();
 	}
 	fwrite(record.num, sizeof(int), i, file);
 	fwrite(try_s, 1, 3, file);
 	/*TRY NUM*/
 	printf("Закончить ввод вопросов?[y]...\n");
-	exit=getch();
+	exit=getchar();
     }
     /*IN WORKING*/
     fclose(file);
