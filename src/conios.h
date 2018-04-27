c1966c8f94303d3fa37960a5017b4f8a96d42de2
SlavaYES <voen1999@gmail.com>
2018-04-27 14:30:20 +0700

    Redesigned the structure of the project

--- src/conios.h
+++
@@ -1,6 +0,0 @@
-void gotoxy(int x, int y);
-void clrscr(void);
-void initTermios(int);
-void resetTermios(void);
-char getch_(int);
-char getch(void);
--- src/main.c
+++ src/main.c
@@ -1,118 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ncurses.h>
-#include "conios.h"
 #include "add_tem.h"
-#define x 100
-#define y 20
-#define xh 70
-#define yh 20
 
 int main()
 {
-    int i;
     char enter;
-    
-    /*CREAT ADDTEST*/
-    set_theme();
-    return 0;
-    /*CREAT ADDTEST*/
-    
-    /*HELP*/
-    gotoxy(xh, yh+1);
-    printf("Вверх:   'W'");
-    gotoxy(xh, yh+2);
-    printf("Вниз:    'S'");
-    gotoxy(xh, yh+3);
-    printf("Выбрать: 'E'");
-    /*MENU*/
-    gotoxy(x, y+1);
-    #ifdef _WIN32
-    settextcolor(15, 0);
-    #else
-    printf("\x1b[1;36m"); //watchig
-    #endif
-    printf("Создать тест");
-    #ifdef _WIN32
-    settextcolor(0, 15);
-    #else
-    printf("\x1b[0m"); //watching
-    #endif
-    gotoxy(x, y+2);
-    printf("Пройти  тест");
-    gotoxy(x, y+3);
-    printf("Помощь");
-    gotoxy(x, y+4);
-    printf("Выйти");
-
-    i=1;
-    gotoxy(x+11, y+i);
-    while (enter!='e') {
-	clrscr();
-	enter=getch();
-	/*HELP*/
-	gotoxy(xh, yh+1);
-	printf("Вверх:   'W'");
-	gotoxy(xh, yh+2);
-	printf("Вниз:    'S'");
-	gotoxy(xh, yh+3);
-	printf("Выбрать: 'E'");
-	/*MENU*/
-	gotoxy(x, y+1);
-	printf("Создать тест");
-	gotoxy(x, y+2);
-	printf("Пройти  тест");
-	gotoxy(x, y+3);
-	printf("Помощь");
-	gotoxy(x, y+4);
-	printf("Выйти");
-	/*CHECK*/
-	if (enter==119) i--;
-	if (enter==115) i++;
-	if (i<1) i=4;
-	if (i>4) i=1;
-	/*SWITCH POINTER*/
-	gotoxy(x, y+i);
-	#ifdef _WIN32
-	settextcolor(0, 15);
-	#else
-	printf("\x1b[1;36m"); //watching
-	#endif
-	switch (i) {
-	case 1: printf("Создать тест"); break;
-	case 2: printf("Пройти  тест"); break;
-	case 3: printf("Помощь"); break;
-	case 4: printf("Выйти"); break;
+    while (enter != '4') {
+	system("clear");
+	printf("\n\t  Menu\n\n");
+	printf("\t1. Creat\n");
+	printf("\t2. Pass\n");
+	printf("\t3. Help\n");
+	printf("\t4. Quit\n");
+	printf("\nEnter number:\n\t\t"); 
+	enter=getchar();
+	switch (enter)
+	{
+	case '1':
+	    set_theme();
+	    break;
+	case '2':
+	    break;
+	case '3':
+	    break;
+	case '4':
+	    return 0;
+	    break;
 	}
-	
-	#ifdef _WIN32
-	settextcolor(0, 15);
-	#else
-	printf("\x1b[0m"); //watching
-	#endif
     }
-    /*SWITCH ITEM*/
-    gotoxy(0, 25);
-    switch (i) {
-    case 1:
-	break; //CREAT
-    case 2:
-	break; //PASS
-    case 3:
-	break; //HELP
-    case 4:
-	return 0; //EXIT
-    }
+    return 0;
 }
-
-
-
-
-
-
-
-
-
 
 
 
