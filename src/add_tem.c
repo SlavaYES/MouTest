f7ea2f490c817be7bbeddc5b04239bd7f385d91b
SlavaYES <voen1999@gmail.com>
2018-04-28 23:28:58 +0700

    Made input in file Test

--- src/add_tem.c
+++ src/add_tem.c
@@ -1,3 +1,4 @@
+#define _OPEN_SYS_ITOA_EXT
 #include <stdio_ext.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,42 +35,52 @@ int set_theme(void)
     fwrite(the_s, 1, 3, file);
     /*THEME*/
     char enter_v, enter_o, enter_t;
-    char quest[128], answer[32];
-    int try;
-    int i=1, j=1, k=1;
+    char quest[128], answer[32], try[16];
+    int i, j, k;
+    char buffer[10], sk=')', t[4]=". ";
+    i=1;
     do {//ENTER QUESTION
 	printf("Enter (%d) the Question:\n\t", i);
 	__fpurge(stdin);
 	fgets(quest, 128, stdin);
 	fwrite(vop_s, 1, 2, file);
+	sprintf(buffer, "%d", i);
+	fwrite(buffer, 1, 1, file);
+	fwrite(t, 1, 2, file);
 	fwrite(quest, 1, strlen(quest), file);
 	fwrite(vop_s, 1, 3, file);
 	i++;
+	fwrite(otv_s, 1, 2 ,file);
+	j=1;
 	do {//ENTER ANSWER
 	    printf("Enter (%d) the Answer:\n\t", j);
 	    __fpurge(stdin);
 	    fgets(answer, 128, stdin);
-	    fwrite(vop_s, 1, 2, file);
+	    sprintf(buffer, "%d", j);
+	    fwrite(buffer, 1, 1, file);
+	    fwrite(&sk, 1, 1, file);
 	    fwrite(answer, 1, strlen(answer), file);
-	    fwrite(vop_s, 1, 3, file);
 	    j++;
 	    printf("\nQuit the Answer?[y]: ");
 	    __fpurge(stdin);
 	    enter_o=getchar();
 	} while (enter_o != 'y');
+	fwrite(otv_s, 1, 3, file);
 	
+	fwrite(try_s, 1, 2, file);
+	k=1;
 	do {//ENTER TRY ANSWER
 	    printf("Enter (%d) the NUM Try Answer:\n\t", k);
 	    __fpurge(stdin);
-	    scanf("%d", &try);
-	    fwrite(try_s, 1, 2, file);
-	    fwrite(&try, 4, 1, file);
-	    fwrite(try_s, 1, 3, file);
+	    fgets(try, 16, stdin);
+	    fwrite(try, 1, 1, file);
 	    k++;
 	    printf("\nQuit the Try Answer?[y]: ");
 	    __fpurge(stdin);
 	    enter_t=getchar();
 	} while (enter_t != 'y');
+	fwrite(try_s, 1, 3, file);
+	
 	printf("\nQuit the Questuion?[y]: ");
 	__fpurge(stdin);
 	enter_v=getchar();
@@ -78,9 +89,13 @@ int set_theme(void)
     fclose(file);
     /*FILE*/
     /*VIEW FILE*/
+    char p;
     file=fopen(nameFileFull, "rb");
     if (!file) {
 	fputs("ERROR\n", stdout);
+    }
+    while (fread(&p, 1, 1, file)) {
+	printf("%c", p);
     }
     __fpurge(stdin);
     getchar();
