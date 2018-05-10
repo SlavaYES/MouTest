6ca6f2ccafe4cceace3bb1650055b929cda65f49
SlavaYES <voen1999@gmail.com>
2018-05-11 01:49:08 +0700

    + files: set answer and set true answer

---
+++ src/setAnswer.c
@@ -0,0 +1,21 @@
+#include "setAnswer.h"
+#include <string.h>
+#include <stdio_ext.h>
+int setAnswer(FILE *file)
+{
+    int j=1;
+    char enter_o='u', answer[128], buffer[10], sk[3]=") ";
+    do {//ENTER ANSWER
+        printf("Enter (%d) the Answer:\n\t", j);
+        __fpurge(stdin);
+        fgets(answer, 128, stdin);
+        sprintf(buffer, "%d", j);
+        fwrite(buffer, 1, 1, file);
+        fwrite(sk, 1, 2, file);
+        fwrite(answer, 1, strlen(answer), file);
+        j++;
+        printf("\nQuit the Answer?[y]: ");
+	scanf("%c", &enter_o);
+    } while (enter_o != 'y');
+    return 1;
+}
